95890081f6609de3c217573742a8206813d082bc
Ajbajibanpm <elenadivino@gmail.com>
2025-12-28 14:36:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// sw.js - Service Worker per notifiche in background
+// sw.js - Versione potenziata
 self.addEventListener('install', (event) => {
     self.skipWaiting();
 });
@@ -7,15 +7,34 @@ self.addEventListener('activate', (event) => {
     event.waitUntil(clients.claim());
 });
 
-// Ascolta i messaggi dall'app principale
 self.addEventListener('message', (event) => {
     if (event.data && event.data.type === 'SHOW_NOTIFICATION') {
-        self.registration.showNotification(event.data.title, {
+        const options = {
             body: event.data.body,
             icon: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png',
-            vibrate: [200, 100, 200],
             badge: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png',
-            tag: 'monster-spawn'
-        });
+            vibrate: [300, 100, 300],
+            data: { url: self.registration.scope }, // Serve per riaprire l'app al clic
+            actions: [
+                { action: 'open', title: 'Apri Oblò' }
+            ]
+        };
+
+        event.waitUntil(
+            self.registration.showNotification(event.data.title, options)
+        );
     }
+});
+
+// Gestisce il clic sulla notifica: riporta l'utente al gioco
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: 'window' }).then((clientList) => {
+            if (clientList.length > 0) {
+                return clientList[0].focus();
+            }
+            return clients.openWindow('/');
+        })
+    );
 });
